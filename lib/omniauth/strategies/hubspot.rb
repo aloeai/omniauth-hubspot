9d5823105db1b18fa978b4740b22201ac5d26d98
Eric Himmelreich <eric@rawsyntax.com>
2017-09-27 09:10:49 -0500

    Load hub id from hubspot token response

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -13,10 +13,43 @@ module OmniAuth
         token_url: 'oauth/v1/token'
       }
 
+      option :name, 'hubspot'
+
+      option :authorize_options, [:scope]
+
+      uid { raw_info['id'] }
+
+      info do
+        prune!({
+          'uid'             => raw_info['user_id'],
+          'hub_id'          => raw_info['hub_id'],
+          'app_id'          => raw_info['app_id'],
+          'token_type'      => raw_info['token_type'],
+          'scopes'          => raw_info['scopes'],
+          'email'           => raw_info['user'],
+          'hub_domain'      => raw_info['hub_domain'],
+        })
+      end
+
+      extra do
+        { 'user' =>  prune!(raw_info) }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/oauth/v1/access-tokens/#{access_token.token}").parsed
+      end
+
       private
 
       def callback_url
         options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
+
+      def prune!(hash)
+        hash.delete_if do |_, value|
+          prune!(value) if value.is_a?(Hash)
+          value.nil? || (value.respond_to?(:empty?) && value.empty?)
+        end
       end
     end
   end
